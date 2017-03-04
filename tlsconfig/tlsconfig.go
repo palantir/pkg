957468c9feadcccd1c16c8e51731c51966acc378
Nick Miyake <nmiyake@palantir.com>
2017-03-04 13:40:02 -0800

    Add tlsconfig package

---
+++ tlsconfig/tlsconfig.go
@@ -0,0 +1,117 @@
+package tlsconfig
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+)
+
+var defaultCipherSuites = []uint16{
+	// This cipher suite is included to enable http/2. For details, see
+	// https://blog.bracelab.com/achieving-perfect-ssl-labs-score-with-go
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+}
+
+// NewClientConfig returns a tls.Config that is suitable to use for a client connection in 2-way TLS connections. The
+// configuration uses the provided key/value pair as the certificate it presents and uses the provided caFiles as the
+// set of root certificate authorities used to validate certificates. If caFiles is nil or empty, the host's root CA set
+// is used. If cipherSuites is nil or empty, defaultCipherSuites is used.
+func NewClientConfig(certFile, keyFile string, caFiles []string, cipherSuites []uint16) (*tls.Config, error) {
+	return create(cipherSuites,
+		withAuthKeyPair(certFile, keyFile),
+		withRootCAs(caFiles),
+	)
+}
+
+// NewServerConfig returns a tls.Config that is suitable to use for a server in 2-way TLS connections. The configuration
+// uses the provided clientCAFiles as the set of certificate authorities used to validate client certificates and
+// authType as the policy the server follows for client TLS authentication. If cipherSuites is nil or empty,
+// defaultCipherSuites is used. The configuration does not store the key and certificate used by the server.
+func NewServerConfig(clientCAFiles []string, authType tls.ClientAuthType, cipherSuites []uint16) (*tls.Config, error) {
+	return create(cipherSuites,
+		withClientConfig(clientCAFiles, authType),
+	)
+}
+
+// BuildCACertPool returns a certificate pool that contains the certificates read from the provided files. Returns an
+// error if any of the provided files cannot be opened or if any of the files contain no certificates.
+func BuildCACertPool(caFiles ...string) (*x509.CertPool, error) {
+	caCertPool := x509.NewCertPool()
+	for _, caFile := range caFiles {
+		cert, err := ioutil.ReadFile(caFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load certificates from file %s: %v", caFile, err)
+		}
+		if ok := caCertPool.AppendCertsFromPEM(cert); !ok {
+			return nil, fmt.Errorf("no certificates detected in file %s", caFile)
+		}
+	}
+	return caCertPool, nil
+}
+
+type configurer func(*tls.Config) error
+
+func withAuthKeyPair(certFile, keyFile string) configurer {
+	return func(tlsConfig *tls.Config) error {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return fmt.Errorf("failed to load certificate from cert file %s and key file %s: %v", certFile, keyFile, err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+		return nil
+	}
+}
+
+func withClientConfig(clientCAFiles []string, authType tls.ClientAuthType) configurer {
+	return func(tlsConfig *tls.Config) error {
+		caCertPool, err := BuildCACertPool(clientCAFiles...)
+		if err != nil {
+			return fmt.Errorf("failed to load client CA certificates: %v", err)
+		}
+		tlsConfig.ClientCAs = caCertPool
+		tlsConfig.ClientAuth = authType
+		return nil
+	}
+}
+
+func withRootCAs(caFiles []string) configurer {
+	return func(tlsConfig *tls.Config) error {
+		if len(caFiles) == 0 {
+			return nil
+		}
+		caCertPool, err := BuildCACertPool(caFiles...)
+		if err != nil {
+			return fmt.Errorf("failed to load root CA certificates: %v", err)
+		}
+		tlsConfig.RootCAs = caCertPool
+		return nil
+	}
+}
+
+func create(cipherSuites []uint16, configs ...configurer) (*tls.Config, error) {
+	tlsConfig := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		CipherSuites:             defaultCipherSuites,
+	}
+	if len(cipherSuites) != 0 {
+		tlsConfig.CipherSuites = cipherSuites
+	}
+
+	for _, currCfg := range configs {
+		if currCfg == nil {
+			continue
+		}
+		if err := currCfg(tlsConfig); err != nil {
+			return nil, err
+		}
+	}
+
+	tlsConfig.BuildNameToCertificate()
+	return tlsConfig, nil
+}
---
+++ tlsconfig/tlsconfig_test.go
@@ -0,0 +1,259 @@
+package tlsconfig_test
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/palantir/pkg/tlsconfig"
+)
+
+const (
+	cert1File         = "testdata/cert-1.pem"
+	key1File          = "testdata/key-1.pem"
+	cert2File         = "testdata/cert-2.pem"
+	combinedCertsFile = "testdata/combined-certs.pem"
+	certWithKeyFile   = "testdata/cert-with-key.pem"
+)
+
+func TestNewClientConfig(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name         string
+		caFiles      []string
+		cipherSuites []uint16
+	}{
+		{
+			name: "defaults",
+		},
+		{
+			name: "caFiles specified",
+			caFiles: []string{
+				cert2File,
+			},
+		},
+		{
+			name: "cipherSuites specified",
+			cipherSuites: []uint16{
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		},
+	} {
+		cfg, err := tlsconfig.NewClientConfig(cert1File, key1File, currCase.caFiles, currCase.cipherSuites)
+		require.NoError(t, err)
+		assert.NotNil(t, cfg, "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+func TestNewClientConfigErrors(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name      string
+		certFile  string
+		keyFile   string
+		caFiles   []string
+		wantError string
+	}{
+		{
+			name:      "missing certificate file",
+			certFile:  "",
+			keyFile:   key1File,
+			wantError: "^failed to load certificate from cert file  and key file .+/key-1.pem: open : no such file or directory$",
+		},
+		{
+			name:      "missing key file",
+			certFile:  cert1File,
+			keyFile:   "",
+			wantError: "^failed to load certificate from cert file .+/cert-1.pem and key file : open : no such file or directory$",
+		},
+		{
+			name:     "invalid CA file",
+			certFile: cert1File,
+			keyFile:  key1File,
+			caFiles: []string{
+				key1File,
+			},
+			wantError: "^failed to load root CA certificates: no certificates detected in file .+/key-1.pem$",
+		},
+	} {
+		cfg, err := tlsconfig.NewClientConfig(currCase.certFile, currCase.keyFile, currCase.caFiles, nil)
+		require.Error(t, err, fmt.Sprintf("Case %d: %s", currCaseNum, currCase.name))
+		assert.Regexp(t, regexp.MustCompile(currCase.wantError), err.Error(), "Case %d: %s", currCaseNum, currCase.name)
+		assert.Nil(t, cfg, "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+func TestNewServerConfig(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name          string
+		clientCAFiles []string
+		authType      tls.ClientAuthType
+		cipherSuites  []uint16
+	}{
+		{
+			name: "defaults",
+		},
+		{
+			name: "caFiles specified",
+			clientCAFiles: []string{
+				cert1File,
+			},
+		},
+		{
+			name:     "authType specified",
+			authType: tls.NoClientCert,
+		},
+		{
+			name: "cipherSuites specified",
+			cipherSuites: []uint16{
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		},
+	} {
+		cfg, err := tlsconfig.NewServerConfig(currCase.clientCAFiles, currCase.authType, currCase.cipherSuites)
+		require.NoError(t, err)
+		assert.NotNil(t, cfg, "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+func TestNewServerConfigErrors(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name          string
+		clientCAFiles []string
+		wantError     string
+	}{
+		{
+			name: "invalid CA file",
+			clientCAFiles: []string{
+				key1File,
+			},
+			wantError: "^failed to load client CA certificates: no certificates detected in file .+/key-1.pem$",
+		},
+	} {
+		cfg, err := tlsconfig.NewServerConfig(currCase.clientCAFiles, tls.NoClientCert, nil)
+		require.Error(t, err, fmt.Sprintf("Case %d: %s", currCaseNum, currCase.name))
+		assert.Regexp(t, regexp.MustCompile(currCase.wantError), err.Error(), "Case %d: %s", currCaseNum, currCase.name)
+		assert.Nil(t, cfg, "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+func TestBuildCACertPool(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name         string
+		inputFiles   []string
+		wantNumCerts int
+	}{
+		{
+			name:         "no files",
+			wantNumCerts: 0,
+		},
+		{
+			name: "file with single certificate",
+			inputFiles: []string{
+				cert1File,
+			},
+			wantNumCerts: 1,
+		},
+		{
+			name: "multiple files with single certificate",
+			inputFiles: []string{
+				cert1File,
+				cert2File,
+			},
+			wantNumCerts: 2,
+		},
+		{
+			name: "single file with multiple certificates",
+			inputFiles: []string{
+				combinedCertsFile,
+			},
+			wantNumCerts: 2,
+		},
+		{
+			name: "single file with certificate and key",
+			inputFiles: []string{
+				certWithKeyFile,
+			},
+			wantNumCerts: 1,
+		},
+	} {
+		certPool, err := tlsconfig.BuildCACertPool(currCase.inputFiles...)
+		require.NoError(t, err, "Case %d: %s", currCaseNum, currCase.name)
+		assert.Equal(t, currCase.wantNumCerts, len(certPool.Subjects()), "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+func TestBuildCACertPoolErrors(t *testing.T) {
+	for currCaseNum, currCase := range []struct {
+		name       string
+		inputFiles []string
+		wantError  string
+	}{
+		{
+			name: "nonexistent file",
+			inputFiles: []string{
+				"nonexistent-file.txt",
+			},
+			wantError: `^failed to load certificates from file nonexistent-file.txt: open nonexistent-file.txt: no such file or directory$`,
+		},
+		{
+			name: "file with no certificates",
+			inputFiles: []string{
+				key1File,
+			},
+			wantError: `^no certificates detected in file .+/key-1.pem$`,
+		},
+		{
+			name: "multiple files where one has no certificates",
+			inputFiles: []string{
+				cert1File,
+				key1File,
+			},
+			wantError: `^no certificates detected in file .+/key-1.pem$`,
+		},
+	} {
+		_, err := tlsconfig.BuildCACertPool(currCase.inputFiles...)
+		require.Error(t, err, fmt.Sprintf("Case %d: %s", currCaseNum, currCase.name))
+		assert.Regexp(t, regexp.MustCompile(currCase.wantError), err.Error(), "Case %d: %s", currCaseNum, currCase.name)
+	}
+}
+
+// newTestKeyPair creates a new self-signed key/certificate pair.
+func newTestKeyPair(serial int64, org, dnsName string) (string, string) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	certTemplate := x509.Certificate{
+		SerialNumber: big.NewInt(serial),
+		Subject:      pkix.Name{Organization: []string{org}},
+		DNSNames:     []string{dnsName},
+	}
+
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	var certBuf bytes.Buffer
+	err = pem.Encode(&certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: certDERBytes})
+	if err != nil {
+		panic(err)
+	}
+	var keyBuf bytes.Buffer
+	err = pem.Encode(&keyBuf, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)})
+	if err != nil {
+		panic(err)
+	}
+	return certBuf.String(), keyBuf.String()
+}
