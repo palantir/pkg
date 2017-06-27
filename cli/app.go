fc30800bbaa7535164376d4618e3d242b95d39c4
Nick Miyake <nmiyake@users.noreply.github.com>
2017-06-26 20:42:51 -0700

    Add ContextOptions and add viper implementation (#61)
    Allows CLI applications to be configured such that actions
    automatically bind their flag values.

--- cli/app.go
+++ cli/app.go
@@ -13,18 +13,21 @@ import (
 
 type App struct {
 	Command
-	Before       func(ctx Context) error
-	ErrorHandler func(ctx Context, err error) int
-	Version      string
-	Stdout       io.Writer
-	Stderr       io.Writer
-	Completion   map[string]completion.Provider
-	Manpage      *Manpage
-	Backcompat   []Backcompat
-	OnExit       OnExit
+	Before         func(ctx Context) error
+	ErrorHandler   func(ctx Context, err error) int
+	Version        string
+	Stdout         io.Writer
+	Stderr         io.Writer
+	Completion     map[string]completion.Provider
+	Manpage        *Manpage
+	Backcompat     []Backcompat
+	OnExit         OnExit
+	ContextOptions []ContextOption
 }
 
 type Option func(*App)
+
+type ContextOption func(*Context)
 
 func NewApp(opts ...Option) *App {
 	app := &App{
