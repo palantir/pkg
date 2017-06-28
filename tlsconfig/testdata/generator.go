1b5758a94ca2a0a6a778c72745b2f18dd05c2512
Nick Miyake <nmiyake@palantir.com>
2017-06-27 18:00:00 -0700

    Change TLS functions
    * Use parameter arguments to configure behavior
    * Change API for server config to store the TLS certificate
      presented by the server
    * Update generator to generate CA and server and client key pairs
      and update tests to test functional behavior

--- tlsconfig/testdata/generator.go
+++ tlsconfig/testdata/generator.go
@@ -13,69 +13,139 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/big"
+	"net"
+	"os"
+	"time"
 )
 
 const (
-	cert1File         = "cert-1.pem"
-	key1File          = "key-1.pem"
-	cert2File         = "cert-2.pem"
+	caCertFile     = "ca-cert.pem"
+	caKeyFile      = "ca-key.pem"
+	serverCertFile = "server-cert.pem"
+	serverKeyFile  = "server-key.pem"
+	clientCertFile = "client-cert.pem"
+	clientKeyFile  = "client-key.pem"
+
 	combinedCertsFile = "combined-certs.pem"
 	certWithKeyFile   = "cert-with-key.pem"
 )
 
+var (
+	serverTemplate = x509.Certificate{
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(10 * 365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+		DNSNames:              []string{"localhost"},
+		IPAddresses:           []net.IP{net.IPv4(127, 0, 0, 1)},
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+	clientTemplate = x509.Certificate{
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(10 * 365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+	}
+)
+
 func main() {
-	cert1, key1 := newTestKeyPair(1, "test", "localhost")
-	err := ioutil.WriteFile(cert1File, []byte(cert1), 0644)
+	caCertStr, caKeyStr := newCAKeyPair(1, "Root CA")
+	block, _ := pem.Decode([]byte(caCertStr))
+	if block == nil {
+		panic("failed to parse certificate PEM")
+	}
+	caCert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(key1File, []byte(key1), 0644)
-	if err != nil {
-		panic(err)
-	}
-	cert2, _ := newTestKeyPair(2, "test", "test.com")
-	err = ioutil.WriteFile(cert2File, []byte(cert2), 0644)
-	if err != nil {
-		panic(err)
-	}
-	combinedCerts := fmt.Sprintf("%s%s", cert1, cert2)
-	err = ioutil.WriteFile(combinedCertsFile, []byte(combinedCerts), 0644)
-	if err != nil {
-		panic(err)
-	}
-	certWithKey := fmt.Sprintf("%s%s", cert1, key1)
-	err = ioutil.WriteFile(certWithKeyFile, []byte(certWithKey), 0644)
-	if err != nil {
+
+	serverCertStr, serverKeyStr := newSignedKeyPair(2, "Test Org", "localhost", &serverTemplate, caCert, caKeyStr)
+	clientCertStr, clientKeyStr := newSignedKeyPair(3, "Test Org", "client", &clientTemplate, caCert, caKeyStr)
+
+	mustWriteFile(caCertFile, []byte(caCertStr), 0644)
+	mustWriteFile(caKeyFile, []byte(caKeyStr), 0644)
+
+	mustWriteFile(serverCertFile, []byte(serverCertStr), 0644)
+	mustWriteFile(serverKeyFile, []byte(serverKeyStr), 0644)
+
+	mustWriteFile(clientCertFile, []byte(clientCertStr), 0644)
+	mustWriteFile(clientKeyFile, []byte(clientKeyStr), 0644)
+
+	combinedCerts := fmt.Sprint(caCertStr, serverCertStr)
+	mustWriteFile(combinedCertsFile, []byte(combinedCerts), 0644)
+	serverCertWithKey := fmt.Sprint(serverCertStr, serverKeyStr)
+	mustWriteFile(certWithKeyFile, []byte(serverCertWithKey), 0644)
+}
+
+func mustWriteFile(filename string, data []byte, perm os.FileMode) {
+	if err := ioutil.WriteFile(filename, data, perm); err != nil {
 		panic(err)
 	}
 }
 
-// newTestKeyPair creates a new self-signed key/certificate pair.
-func newTestKeyPair(serial int64, org, dnsName string) (string, string) {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		panic(err)
-	}
-
-	certTemplate := x509.Certificate{
+func newCAKeyPair(serial int64, org string) (string, string) {
+	caTemplate := &x509.Certificate{
 		SerialNumber: big.NewInt(serial),
-		Subject:      pkix.Name{Organization: []string{org}},
-		DNSNames:     []string{dnsName},
+		Subject: pkix.Name{
+			Organization: []string{org},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(10 * 365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+		IsCA:        true,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
 	}
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &privKey.PublicKey, privKey)
+	if err != nil {
+		panic(err)
+	}
+	keyDERBytes := x509.MarshalPKCS1PrivateKey(privKey)
+	return derBytesToStrings(certDERBytes, keyDERBytes)
+}
 
-	certDERBytes, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &privateKey.PublicKey, privateKey)
+func newSignedKeyPair(serial int64, org, cn string, template, caCert *x509.Certificate, caPrivKey string) (string, string) {
+	block, _ := pem.Decode([]byte(caPrivKey))
+	if block == nil {
+		panic("failed to parse certificate PEM")
+	}
+	signingPrivKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
 		panic(err)
 	}
 
-	var certBuf bytes.Buffer
-	err = pem.Encode(&certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: certDERBytes})
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
+		panic(err)
+	}
+
+	tmplCopy := *template
+	tmplCopy.SerialNumber = big.NewInt(serial)
+	tmplCopy.Subject = pkix.Name{
+		Organization: []string{org},
+		CommonName:   cn,
+	}
+
+	certDERBytes, err := x509.CreateCertificate(rand.Reader, &tmplCopy, caCert, &privKey.PublicKey, signingPrivKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return derBytesToStrings(certDERBytes, x509.MarshalPKCS1PrivateKey(privKey))
+}
+
+func derBytesToStrings(certDERBytes, keyDERBytes []byte) (string, string) {
+	var certBuf bytes.Buffer
+	if err := pem.Encode(&certBuf, &pem.Block{Type: "CERTIFICATE", Bytes: certDERBytes}); err != nil {
 		panic(err)
 	}
 	var keyBuf bytes.Buffer
-	err = pem.Encode(&keyBuf, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)})
-	if err != nil {
+	if err := pem.Encode(&keyBuf, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: keyDERBytes}); err != nil {
 		panic(err)
 	}
 	return certBuf.String(), keyBuf.String()
