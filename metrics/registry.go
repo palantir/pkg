dca1a6f206c15cb45a317c8ac4795b3a58057f94
Nick Miyake <nmiyake@palantir.com>
2018-11-15 15:24:53 -0800

    Fix documentation on function

--- metrics/registry.go
+++ metrics/registry.go
@@ -81,8 +81,7 @@ type Registry interface {
 	Unregister(name string, tags ...Tag)
 }
 
-// NewRootMetricsRegistry creates a new root registry for metrics. This call also starts a goroutine that captures Go
-// runtime information as metrics at the specified frequency.
+// NewRootMetricsRegistry creates a new root registry for metrics.
 func NewRootMetricsRegistry() RootRegistry {
 	return &rootRegistry{
 		registry:           metrics.NewRegistry(),
