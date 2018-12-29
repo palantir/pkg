df0f2009d6b36142caee6938862040aa8d0133f3
Brad Moylan <bmoylan@palantir.com>
2018-12-28 20:29:02 -0800

    bearer token implements text (un)marshal

--- bearertoken/bearertoken.go
+++ bearertoken/bearertoken.go
@@ -7,3 +7,12 @@ package bearertoken
 // Token represents a bearer token, generally sent by a REST client in a
 // Authorization or Cookie header for authentication purposes.
 type Token string
+
+func (t Token) MarshalText() ([]byte, error) {
+	return []byte(t), nil
+}
+
+func (t *Token) UnmarshalText(text []byte) error {
+	*t = Token(text)
+	return nil
+}
