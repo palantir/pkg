332184900c68094416680529ecbe2592135374b6
Brad Moylan <moylan.brad@gmail.com>
2019-03-19 15:27:43 -0700

    metrics.AddTags should not mutate the input context (#153)

--- metrics/context_test.go
+++ metrics/context_test.go
@@ -23,19 +23,37 @@ func TestFromContext(t *testing.T) {
 	assert.Equal(t, FromContext(ctx), reg)
 }
 
-func TestFromContextWithTags(t *testing.T) {
+func TestFromContextAddTags(t *testing.T) {
 	ctx := context.Background()
 	reg := &rootRegistry{
 		registry: metrics.NewPrefixedRegistry("foo"),
 	}
 
 	ctx = WithRegistry(ctx, reg)
+
+	assert.Equal(t, reg, FromContext(ctx))
+	assert.Equal(t, Tags(nil), TagsFromContext(ctx))
+
 	ctx = AddTags(ctx, MustNewTag("bar", "baz"))
 
-	assert.Equal(t, FromContext(ctx), &childRegistry{
+	assert.Equal(t, &childRegistry{
 		root: reg,
 		tags: Tags{MustNewTag("bar", "baz")},
-	})
+	}, FromContext(ctx))
+	assert.Equal(t, Tags{MustNewTag("bar", "baz")}, TagsFromContext(ctx))
+
+	ctx1 := AddTags(ctx, MustNewTag("qux", "quux"))
+
+	// Verify original context was not modified
+	assert.Equal(t, Tags{MustNewTag("bar", "baz")}, TagsFromContext(ctx))
+
+	assert.Equal(t, &childRegistry{
+		root: reg,
+		tags: Tags{MustNewTag("bar", "baz"), MustNewTag("qux", "quux")},
+	}, FromContext(ctx1))
+
+	assert.Equal(t, Tags{MustNewTag("bar", "baz"), MustNewTag("qux", "quux")}, TagsFromContext(ctx1))
+
 }
 
 func TestDefaultFromContext(t *testing.T) {
