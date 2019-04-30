17ecb7f75841cc15fc3bf522203539476268c821
Kevin Simons <ksimons@palantir.com>
2019-04-30 15:29:10 -0700

    Change package name

--- typenames/names.go
+++ typenames/names.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package types
+// Package typenames generates human-friendly names for go objects using reflection.
+package typenames
 
 import (
 	"fmt"
