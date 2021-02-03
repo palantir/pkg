5c678c0999b5a3f86f5a3075f38d4e6e4dc47df0
Brad Moylan <bmoylan@palantir.com>
2021-02-02 16:21:18 -0800

    refreshable: Add Map and Subscribe methods to builtins (#201)
    * refreshable: Add Map and Subscribe methods to builtins
    
    * dont forget duration pointers!
    
    * fix

--- refreshable/refreshable_types.go
+++ refreshable/refreshable_types.go
@@ -11,47 +11,65 @@ import (
 type String interface {
 	Refreshable
 	CurrentString() string
+	MapString(func(string) interface{}) Refreshable
+	SubscribeToString(func(string)) (unsubscribe func())
 }
 
 type StringPtr interface {
 	Refreshable
 	CurrentStringPtr() *string
+	MapStringPtr(func(*string) interface{}) Refreshable
+	SubscribeToStringPtr(func(*string)) (unsubscribe func())
 }
 
 type StringSlice interface {
 	Refreshable
 	CurrentStringSlice() []string
+	MapStringSlice(func([]string) interface{}) Refreshable
+	SubscribeToStringSlice(func([]string)) (unsubscribe func())
 }
 
 type Int interface {
 	Refreshable
 	CurrentInt() int
+	MapInt(func(int) interface{}) Refreshable
+	SubscribeToInt(func(int)) (unsubscribe func())
 }
 
 type IntPtr interface {
 	Refreshable
 	CurrentIntPtr() *int
+	MapIntPtr(func(*int) interface{}) Refreshable
+	SubscribeToIntPtr(func(*int)) (unsubscribe func())
 }
 
 type Bool interface {
 	Refreshable
 	CurrentBool() bool
+	MapBool(func(bool) interface{}) Refreshable
+	SubscribeToBool(func(bool)) (unsubscribe func())
 }
 
 type BoolPtr interface {
 	Refreshable
 	CurrentBoolPtr() *bool
+	MapBoolPtr(func(*bool) interface{}) Refreshable
+	SubscribeToBoolPtr(func(*bool)) (unsubscribe func())
 }
 
 // Duration is a Refreshable that can return the current time.Duration.
 type Duration interface {
 	Refreshable
 	CurrentDuration() time.Duration
+	MapDuration(func(time.Duration) interface{}) Refreshable
+	SubscribeToDuration(func(time.Duration)) (unsubscribe func())
 }
 
 type DurationPtr interface {
 	Refreshable
 	CurrentDurationPtr() *time.Duration
+	MapDurationPtr(func(*time.Duration) interface{}) Refreshable
+	SubscribeToDurationPtr(func(*time.Duration)) (unsubscribe func())
 }
 
 func NewBool(in Refreshable) Bool {
@@ -127,34 +145,142 @@ func (rt refreshableTyped) CurrentString() string {
 	return rt.Current().(string)
 }
 
+func (rt refreshableTyped) MapString(mapFn func(string) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(string))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToString(subFn func(string)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(string))
+	})
+}
+
 func (rt refreshableTyped) CurrentStringPtr() *string {
 	return rt.Current().(*string)
+}
+
+func (rt refreshableTyped) MapStringPtr(mapFn func(*string) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(*string))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToStringPtr(subFn func(*string)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(*string))
+	})
 }
 
 func (rt refreshableTyped) CurrentStringSlice() []string {
 	return rt.Current().([]string)
 }
 
+func (rt refreshableTyped) MapStringSlice(mapFn func([]string) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.([]string))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToStringSlice(subFn func([]string)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.([]string))
+	})
+}
+
 func (rt refreshableTyped) CurrentInt() int {
 	return rt.Current().(int)
+}
+
+func (rt refreshableTyped) MapInt(mapFn func(int) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(int))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToInt(subFn func(int)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(int))
+	})
 }
 
 func (rt refreshableTyped) CurrentIntPtr() *int {
 	return rt.Current().(*int)
 }
 
+func (rt refreshableTyped) MapIntPtr(mapFn func(*int) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(*int))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToIntPtr(subFn func(*int)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(*int))
+	})
+}
+
 func (rt refreshableTyped) CurrentBool() bool {
 	return rt.Current().(bool)
+}
+
+func (rt refreshableTyped) MapBool(mapFn func(bool) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(bool))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToBool(subFn func(bool)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(bool))
+	})
 }
 
 func (rt refreshableTyped) CurrentBoolPtr() *bool {
 	return rt.Current().(*bool)
 }
 
+func (rt refreshableTyped) MapBoolPtr(mapFn func(*bool) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(*bool))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToBoolPtr(subFn func(*bool)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(*bool))
+	})
+}
+
 func (rt refreshableTyped) CurrentDuration() time.Duration {
 	return rt.Current().(time.Duration)
 }
 
+func (rt refreshableTyped) MapDuration(mapFn func(time.Duration) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(time.Duration))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToDuration(subFn func(time.Duration)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(time.Duration))
+	})
+}
+
 func (rt refreshableTyped) CurrentDurationPtr() *time.Duration {
 	return rt.Current().(*time.Duration)
+}
+
+func (rt refreshableTyped) MapDurationPtr(mapFn func(*time.Duration) interface{}) Refreshable {
+	return rt.Map(func(i interface{}) interface{} {
+		return mapFn(i.(*time.Duration))
+	})
+}
+
+func (rt refreshableTyped) SubscribeToDurationPtr(subFn func(*time.Duration)) (unsubscribe func()) {
+	return rt.Subscribe(func(i interface{}) {
+		subFn(i.(*time.Duration))
+	})
 }
