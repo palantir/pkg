8fafe13322ae2d15ad3169228ec25bbdd5b9ce96
Brad Moylan <bmoylan@palantir.com>
2023-11-02 09:08:36 -0700

    FromV2

--- refreshable/v2_test.go
+++ refreshable/v2_test.go
@@ -10,10 +10,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestToV2(t *testing.T) {
-	v1 := NewDefaultRefreshable("original")
-	v2 := ToV2[string](v1)
-	assert.Equal(t, v2.Current(), "original")
-	assert.NoError(t, v1.Update("updated"))
-	assert.Equal(t, v2.Current(), "updated")
+func TestToFromV2(t *testing.T) {
+	base := NewDefaultRefreshable("original")
+	v2 := ToV2[string](base)
+	v1 := FromV2(v2)
+	assert.Equal(t, base.Current(), "original", "base missing original value")
+	assert.Equal(t, v2.Current(), "original", "v2 missing original value")
+	assert.Equal(t, v1.Current(), "original", "v1 missing original value")
+
+	assert.NoError(t, base.Update("updated"))
+	assert.Equal(t, base.Current(), "updated", "base missing updated value")
+	assert.Equal(t, v2.Current(), "updated", "v2 missing updated value")
+	assert.Equal(t, v1.Current(), "updated", "v1 missing updated value")
 }
