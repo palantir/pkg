2529abb0b19227bdf62ac9e6da0b6062b3fee057
Devin Trejo <dtrejo@palantir.com>
2024-03-07 16:39:45 -0500

    Update set of ciphers to latest set.

--- tlsconfig/common.go
+++ tlsconfig/common.go
@@ -12,11 +12,19 @@ import (
 )
 
 var defaultCipherSuites = []uint16{
-	// This cipher suite is included to enable http/2. For details, see
-	// https://blog.bracelab.com/achieving-perfect-ssl-labs-score-with-go
-	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+	// TLSv1.3
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_AES_128_GCM_SHA256,
+	tls.TLS_AES_256_GCM_SHA384,
+	// TLSv1.2
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	// This cipher suite is included to enable http/2. For details, see
+	// required to include for http/2: https://http2.github.io/http2-spec/#rfc.section.9.2.2
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 }
 
 // TLSCertFromFiles returns a provider that returns a tls.Certificate by loading an X509 key pair from the files in the
@@ -89,7 +97,7 @@ func configureTLSConfig(cfgs ...configurer) (*tls.Config, error) {
 	tlsCfg := &tls.Config{
 		MinVersion:               tls.VersionTLS12,
 		PreferServerCipherSuites: true,
-		CipherSuites:             defaultCipherSuites,
+		CipherSuites:             append(defaultCipherSuites, defaultChachaCiphers...),
 		Renegotiation:            tls.RenegotiateNever,
 	}
 	for _, currCfg := range cfgs {
