b2d2a05d33994412e8a987b7c826b56071fd305a
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2025-05-16 14:27:19 -0600

    Failing test

--- refreshable/v2_test.go
+++ refreshable/v2_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestToFromV2(t *testing.T) {
@@ -22,4 +23,19 @@ func TestToFromV2(t *testing.T) {
 	assert.Equal(t, base.Current(), "updated", "base missing updated value")
 	assert.Equal(t, v2.Current(), "updated", "v2 missing updated value")
 	assert.Equal(t, v1.Current(), "updated", "v1 missing updated value")
+}
+
+func TestToV2RespectsUpdates(t *testing.T) {
+	for i := 0; i < 10000; i++ {
+		v1 := NewDefaultRefreshable(1)
+		updateCalled := make(chan struct{})
+		go func() {
+			require.NoError(t, v1.Update(2))
+			close(updateCalled)
+		}()
+		v2 := ToV2[int](v1)
+		<-updateCalled
+		assert.Equal(t, 2, v2.Current(),
+			"v2 should always be updated after Update is called")
+	}
 }
