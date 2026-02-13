59b60d02a2cd97cc67d3ad358bcd0f9760bf2ee1
Brad Moylan <bmoylan@palantir.com>
2026-02-12 17:21:54 -0800

    bench

--- metrics/context_test.go
+++ metrics/context_test.go
@@ -60,3 +60,23 @@ func TestDefaultFromContext(t *testing.T) {
 	ctx := context.Background()
 	assert.Equal(t, FromContext(ctx), DefaultMetricsRegistry)
 }
+
+func BenchmarkFromContext(b *testing.B) {
+	b.ReportAllocs()
+	registry := NewRootMetricsRegistry()
+	ctx := context.Background()
+	ctx = WithRegistry(ctx, registry)
+	ctx = AddTags(ctx, MustNewTag("foo", "bar"))
+	ctx = AddTags(ctx, MustNewTag("bar", "baz"))
+	ctx = AddTags(ctx, MustNewTag("qux", "quux"))
+	ctx = context.WithValue(ctx, "other1", "other1")
+	ctx = context.WithValue(ctx, "other2", "other2")
+
+	for b.Loop() {
+		r := FromContext(ctx)
+		_ = r
+	}
+
+	tags := FromContext(ctx).(*childRegistry).tags
+	assert.Equal(b, Tags{MustNewTag("foo", "bar"), MustNewTag("bar", "baz"), MustNewTag("qux", "quux")}, tags)
+}
