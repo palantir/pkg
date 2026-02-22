14d6c01aa279f063662e260eb54d200972d88952
Kevin Simons <ksimons@palantir.com>
2026-02-22 14:28:05 -0800

    so fast

--- refreshable/refreshable_file_test.go
+++ refreshable/refreshable_file_test.go
@@ -5,6 +5,7 @@
 package refreshable
 
 import (
+	"context"
 	"errors"
 	"os"
 	"path/filepath"
@@ -14,6 +15,13 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+)
+
+const (
+	testStr1              = "renderConf1"
+	testStr2              = "renderConf2"
+	refreshableSyncPeriod = time.Millisecond * 100
+	sleepPeriod           = time.Millisecond * 175
 )
 
 func TestNewFileRefreshable(t *testing.T) {
@@ -213,4 +221,93 @@ func TestNewMultiFileRefreshableCanMap(t *testing.T) {
 	assert.True(t, contents["content1"])
 	assert.True(t, contents["content2"])
 	assert.True(t, contents["additional"])
+}
+
+// Verifies that a RefreshableFile can follow a symlink when the original file updates
+// We ensure we wait long enough (80ms) such that the refreshable duration (50ms) will read the change
+func TestRefreshableFileCanFollowSymLink(t *testing.T) {
+	tempDir := t.TempDir()
+	// We will have fileToWritePointingAtActual point at fileToWriteActual
+	fileToWriteActual := filepath.Join(tempDir, "fileToWriteActual")
+	fileToWritePointingAtActual := filepath.Join(tempDir, "fileToWritePointingAtActual")
+	// Write the old file
+	require.NoError(t, os.WriteFile(fileToWriteActual, []byte(testStr1), 0644))
+	// Symlink the old file to point at the new file
+	err := os.Symlink(fileToWriteActual, fileToWritePointingAtActual)
+	assert.NoError(t, err)
+	// Point the refreshable towards the new file
+	r := NewFileRefreshableWithTicker(context.Background(), fileToWritePointingAtActual, time.Tick(refreshableSyncPeriod))
+	str := getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf1")
+	// Update the actual file
+	require.NoError(t, os.WriteFile(fileToWriteActual, []byte(testStr2), 0644))
+	time.Sleep(sleepPeriod)
+	str = getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf2")
+}
+
+// Verifies that a RefreshableFile can follow a symlink to a symlink when the original file updates
+// We ensure we wait long enough (80ms) such that the refreshable duration (50ms) will read the change
+func TestRefreshableFileCanFollowMultipleSymLinks(t *testing.T) {
+	tempDir := t.TempDir()
+	// We will have fileToWritePointingAtActual point at fileToWriteActual
+	fileToWriteActual := filepath.Join(tempDir, "fileToWriteActual")
+	fileToWritePointingAtActual := filepath.Join(tempDir, "fileToWritePointingAtActual")
+	fileToWritePointingAtSymlink := filepath.Join(tempDir, "fileToWritePointingAtSymlink")
+	// Write the old file
+	require.NoError(t, os.WriteFile(fileToWriteActual, []byte(testStr1), 0644))
+	// Symlink the old file to point at the new file
+	err := os.Symlink(fileToWriteActual, fileToWritePointingAtActual)
+	assert.NoError(t, err)
+	// Symlink a to a symlink
+	err = os.Symlink(fileToWritePointingAtActual, fileToWritePointingAtSymlink)
+	assert.NoError(t, err)
+	// Point the refreshable towards the new file
+	r := NewFileRefreshableWithTicker(context.Background(), fileToWritePointingAtSymlink, time.Tick(refreshableSyncPeriod))
+	str := getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf1")
+	// Update the symlink file
+	require.NoError(t, os.WriteFile(fileToWriteActual, []byte(testStr2), 0644))
+	time.Sleep(sleepPeriod)
+	str = getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf2")
+}
+
+// Verifies that a RefreshableFile can follow a symlink to a symlink when the symlink changes
+// We ensure we wait long enough (80ms) such that the refreshable duration (50ms) will read the change
+func TestRefreshableFileCanFollowMovingSymLink(t *testing.T) {
+	tempDir := t.TempDir()
+	// We will have fileToWritePointingAtActual point at fileToWriteActual
+	fileToWriteActualOriginal := filepath.Join(tempDir, "fileToWriteActualOriginal")
+	fileToWriteActualUpdated := filepath.Join(tempDir, "fileToWriteActualUpdated")
+	fileToWritePointingAtActual := filepath.Join(tempDir, "fileToWritePointingAtActual")
+	fileToWritePointingAtSymlink := filepath.Join(tempDir, "fileToWritePointingAtSymlink")
+	// Write the old file
+	require.NoError(t, os.WriteFile(fileToWriteActualOriginal, []byte(testStr1), 0644))
+	// Write the old file
+	require.NoError(t, os.WriteFile(fileToWriteActualUpdated, []byte(testStr2), 0644))
+	// Symlink the old file to point at the new file
+	err := os.Symlink(fileToWriteActualOriginal, fileToWritePointingAtActual)
+	assert.NoError(t, err)
+	// Symlink a to a symlink
+	err = os.Symlink(fileToWritePointingAtActual, fileToWritePointingAtSymlink)
+	assert.NoError(t, err)
+	// Point the refreshable towards the new file
+	r := NewFileRefreshableWithTicker(context.Background(), fileToWritePointingAtSymlink, time.Tick(refreshableSyncPeriod))
+	str := getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf1")
+	// Change where the symlink points
+	err = os.Remove(fileToWritePointingAtActual)
+	assert.NoError(t, err)
+	err = os.Symlink(fileToWriteActualUpdated, fileToWritePointingAtActual)
+	assert.NoError(t, err)
+
+	// Update the symlink file
+	time.Sleep(sleepPeriod)
+	str = getStringFromRefreshable(t, r)
+	assert.Equal(t, str, "renderConf2")
+}
+
+func getStringFromRefreshable(t *testing.T, r Refreshable[[]byte]) string {
+	return string(r.Current())
 }
