85a30e7ebcf7e4a685f32ae63a7d9e4fa9f968ef
Nick Miyake <nmiyake@palantir.com>
2026-08-12 11:58:24 -0700

    Update

--- .circleci/template.sh
+++ .circleci/template.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export CIRCLECI_TEMPLATE=go-library-oss
 export PRIMARY_BRANCH=master
+export USE_GOPATH_WD=true
 export MODULES=bearertoken,binary,boolean,bytesbuffers,cli,clipackager,cobracli,datetime,gittest,httpclient,httpserver,matcher,merge,metrics,objmatcher,pkgpath,refreshable,retry,rid,safehttp,safejson,safelong,safeyaml,signals,specdir,tableprinter,tlsconfig,transform,typenames,uuid,yamlpatch
