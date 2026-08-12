f570818ade46d88c6d3a827ea17aada5a9814ff8
Nick Miyake <nmiyake@palantir.com>
2026-08-12 11:17:17 -0700

    Test template

--- .circleci/generate.go
+++ .circleci/generate.go
@@ -3,6 +3,10 @@
 
 // This program prints the CircleCI configuration for the "pkg" repository. Standard way to run it is to run
 // "go run generate.go ../ > config.yml" from the directory that contains this file (corresponds to "go run generate.go {{parentDir}} > config.yml").
+//
+// It can also print the "template.sh" that renders an equivalent configuration using the shared "go-library-oss"
+// CircleCI template by running "go run generate.go ../ template.sh > template.sh" from the directory that contains this
+// file (corresponds to "go run generate.go {{parentDir}} template.sh > template.sh").
 package main
 
 import (
@@ -12,6 +16,11 @@ import (
 	"os"
 	"strings"
 	"text/template"
+)
+
+const (
+	configYMLOutput  = "config.yml"
+	templateShOutput = "template.sh"
 )
 
 const (
@@ -98,6 +107,16 @@ workflows:
                       <<: *checkout-path
           <<: *all-tags-filter
 `
+
+	// Every module of this repository is a peer in its own directory and the root of the repository is not a godel
+	// project, so all of the modules are listed in "MODULES": "ADDITIONAL_MODULES" would render jobs for a module at
+	// the root of the repository that does not exist. "PRIMARY_BRANCH" is set because the template defaults to
+	// "develop", while this repository uses "master".
+	templateShTemplateContent = `#!/usr/bin/env bash
+export CIRCLECI_TEMPLATE=go-library-oss
+export PRIMARY_BRANCH=master
+export MODULES={{.Modules}}
+`
 )
 
 type TemplateObject struct {
@@ -109,22 +128,35 @@ func main() {
 		panic("parent directory must be provided as argument")
 	}
 	modParentDir := os.Args[1]
+	output := configYMLOutput
+	if len(os.Args) > 2 {
+		output = os.Args[2]
+	}
 	mods, err := modules(modParentDir)
 	if err != nil {
 		panic(err)
 	}
-	configYML, err := createConfigYML(mods)
+	var content string
+	switch output {
+	case configYMLOutput:
+		content, err = createConfigYML(mods)
+	case templateShOutput:
+		content, err = createTemplateSh(mods)
+	default:
+		panic(fmt.Sprintf("output must be %q or %q, was %q", configYMLOutput, templateShOutput, output))
+	}
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(configYML)
+	fmt.Print(content)
 }
 
 var (
 	executorTemplate,
 	requiresTemplate,
 	jobsWorkflowsTemplate,
-	moduleTemplate *template.Template
+	moduleTemplate,
+	templateShTemplate *template.Template
 )
 
 func init() {
@@ -144,6 +176,10 @@ func init() {
 	moduleTemplate, err = template.New("moduleTemplate").Parse(moduleTemplateContent)
 	if err != nil {
 		panic(fmt.Sprintf("failed to create moduleTemplate template: %v", err))
+	}
+	templateShTemplate, err = template.New("templateShTemplate").Parse(templateShTemplateContent)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create templateShTemplate template: %v", err))
 	}
 }
 
@@ -185,6 +221,16 @@ func createConfigYML(modDirs []string) (string, error) {
 		if err := moduleTemplate.Execute(outBuf, modTemplate); err != nil {
 			return "", fmt.Errorf("failed to execute moduleTemplate template: %v", err)
 		}
+	}
+	return outBuf.String(), nil
+}
+
+func createTemplateSh(modDirs []string) (string, error) {
+	outBuf := &bytes.Buffer{}
+	if err := templateShTemplate.Execute(outBuf, map[string]interface{}{
+		"Modules": strings.Join(modDirs, ","),
+	}); err != nil {
+		return "", fmt.Errorf("failed to execute templateShTemplate template: %v", err)
 	}
 	return outBuf.String(), nil
 }
